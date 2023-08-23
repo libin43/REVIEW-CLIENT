b666e24e704079138f151243c3bcc74245fc45bd
Libin Biji <libinbiji43@gmail.com>
2023-08-23 13:45:08 +0530

    Initial Commit

---
+++ src/pages/user/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavMenu from '../../../components/NavMenu'
+import './HomePage.css'
+
+const HomePage = () => {
+  return (
+    <div>
+        <NavMenu/>
+        Welocme home
+    </div>
+  )
+}
+
+export default HomePage
---
+++ src/pages/user/ViewBookPage/ViewBookPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavMenu from '../../../components/NavMenu'
+import TableView from '../../../components/TableView'
+
+const ViewBookPage = () => {
+  return (
+    <div>
+        <NavMenu/>
+        <TableView/>
+    </div>
+  )
+}
+
+export default ViewBookPage
